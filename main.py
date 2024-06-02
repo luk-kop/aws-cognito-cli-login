a612b94e86cdffe562144ec444f9d7c5f5a15afb
luk-kop <luk.kopicki@gmail>
2024-06-02 19:15:33 +0200

    Added MFA app support

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import boto3
 from botocore.exceptions import ClientError
 from pwinput import pwinput
 from dotenv import load_dotenv
+import segno
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 logger = logging.getLogger()
@@ -45,10 +46,9 @@ if __name__ == '__main__':
         sys.exit(1)
 
     if 'ChallengeName' in auth_response:
-        # TODO: add challenge response support
+        # TODO: while loop for challenge
         challenge_name: str = auth_response['ChallengeName']
-        # TODO: add supported challenges (enum)
-        supported_challenges = ['NEW_PASSWORD_REQUIRED', 'MFA_SETUP']
+        print(challenge_name)
         if challenge_name == 'NEW_PASSWORD_REQUIRED':
             new_password: str = pwinput(prompt='New password: ')
             challenge_responses = {
@@ -56,17 +56,59 @@ if __name__ == '__main__':
                 'NEW_PASSWORD': new_password
             }
         elif challenge_name == 'MFA_SETUP':
-            session_id = input('MFA setup - enter session id: ')
+            session = auth_response['Session']
+            response = client_idp.associate_software_token(
+                # AccessToken=cognito_access_token,
+                Session=session
+            )
+
+            mfa_secret_code: str = response['SecretCode']
+            otpauth = f'otpauth://totp/cognito-test:{username}?secret={mfa_secret_code}&issuer={COGNITO_URL}'
+            mfa_session: str = response['Session']
+
+            print('Scan this QR code with your authenticator app')
+            qrcode = segno.make(otpauth)
+            qrcode.terminal(compact=True)
+            print(f'Alternatively, you can manually enter secret key in your authenticator app - {mfa_secret_code}')
+
+            # TODO: add regex validation - [0-9]+
+            user_code = input('Enter a code from your authenticator app: ')
+            mfa_friendly_device_name = 'MFA-test'
+
+            try:
+                response = client_idp.verify_software_token(
+                    Session=mfa_session,
+                    UserCode=user_code,
+                    FriendlyDeviceName=mfa_friendly_device_name
+                )
+            except ClientError as error:
+                logger.error(error.response['Error']['Message'])
+                sys.exit(1)
+            session: str = response['Session']
             challenge_responses = {
                 'USERNAME': username,
-                'SESSION': session_id
+                'SESSION': session
             }
-        response = client_idp.respond_to_auth_challenge(
-            ClientId=COGNITO_CLIENT_ID,
-            ChallengeName=challenge_name,
-            Session=auth_response['Session'],
-            ChallengeResponses=challenge_responses
-        )
+        elif challenge_name == 'SOFTWARE_TOKEN_MFA':
+            user_code = input('Enter a code from your authenticator app: ')
+            challenge_responses = {
+                'USERNAME': username,
+                'SOFTWARE_TOKEN_MFA_CODE': user_code
+            }
+        else:
+            logger.error(f'Challenge "{challenge_name}" is not supported')
+            sys.exit(1)
+        try:
+            auth_response = client_idp.respond_to_auth_challenge(
+                ClientId=COGNITO_CLIENT_ID,
+                ChallengeName=challenge_name,
+                Session=auth_response['Session'],
+                ChallengeResponses=challenge_responses
+            )
+        except ClientError as error:
+            logger.error(error.response['Error']['Message'])
+            sys.exit(1)
+
     cognito_id_token = auth_response['AuthenticationResult']['IdToken']
     try:
         id_response = client_identity.get_id(
@@ -98,4 +140,3 @@ if __name__ == '__main__':
     }
     for key, value in exports.items():
         print(f'export {key}={value}')
-
